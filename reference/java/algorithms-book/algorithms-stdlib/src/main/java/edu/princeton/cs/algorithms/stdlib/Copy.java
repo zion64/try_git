c3829ca54b86d7a34c1ae5b195ff9b2f874a8920
Carlos Adolfo Ortiz Quiros <carlosaortiz@yahoo.com>
2012-08-23 17:36:19 -0500

    Added new files to library as a port from what the book originaly had

---
+++ reference/java/algorithms-book/algorithms-stdlib/src/main/java/edu/princeton/cs/algorithms/stdlib/Copy.java
@@ -0,0 +1,25 @@
+/*************************************************************************
+ *  Compilation:  javac Copy.java
+ *  Execution:    java Copy < file
+ *  Dependencies: BinaryStdIn.java BinaryStdOut.java
+ *  
+ *  Reads in a binary file from standard input and writes it to standard output.
+ *
+ *  % java Copy < mandrill.jpg > copy.jpg
+ *
+ *  %  diff mandrill.jpg copy.jpg
+ *
+ *************************************************************************/
+
+package edu.princeton.cs.algorithms.stdlib;
+
+public class Copy {
+
+	public static void main(String[] args) {
+		while (!BinaryStdIn.isEmpty()) {
+			char c = BinaryStdIn.readChar();
+			BinaryStdOut.write(c);
+		}
+		BinaryStdOut.flush();
+	}
+}
