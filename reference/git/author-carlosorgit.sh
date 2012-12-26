16314ab64a6453ce141792667fb80ebbdeb9deee
Carlos Adolfo Ortiz Quiros <carlosaortiz@yahoo.com>
2012-12-26 13:22:32 -0500

    This commit is issued to be a brain refresher on SSH key setup both for usege in one computers but using two accounts.
    Read the file setup.txt for instructions on making this step and
    use the help of https://gist.github.com/2351996 for reference, but two
    command shell scripts are needed to sign your commits correctly.
    
    Use of author-carlosorgit.sh will sign as one user connected to default
    host and author-cortizqgithub.sh will sign as the other user connected to
    the non-default host.
    
    Study files carefully and follow instructions for its full understanding.

---
+++ reference/git/author-carlosorgit.sh
@@ -0,0 +1,2 @@
+exec git config --global user.name "Carlos Adolfo Ortiz Quiros (EC)"
+exec git config --global user.email carlosor@elcolombiano.com.co
---
+++ reference/git/author-cortizqgithub.sh
@@ -0,0 +1,2 @@
+exec git config --global user.name "Carlos Adolfo Ortiz Quiros"
+exec git config --global user.email carlosaortiz@yahoo.com
