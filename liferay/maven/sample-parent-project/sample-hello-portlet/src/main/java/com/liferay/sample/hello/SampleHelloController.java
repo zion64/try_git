8911c2bac1b115667689d5a43f621f401997af28
Carlos Adolfo Ortiz Quiros (EC) <carlosaortiz@yahoo.com>
2013-07-10 14:10:38 -0500

    Documentation and typo corrections.

--- liferay/maven/sample-parent-project/sample-hello-portlet/src/main/java/com/liferay/sample/hello/SampleHelloController.java
+++ liferay/maven/sample-parent-project/sample-hello-portlet/src/main/java/com/liferay/sample/hello/SampleHelloController.java
@@ -1,4 +1,10 @@
-/* http://portalhub.wordpress.com/2012/05/12/spring-mvc-portlet-hello-world-based-on-annotations/*/
+/* Jul.09/2013
+ * If you want to use Spring Web MVC Porlet then follow the instructions
+ * contained in this article  http://portalhub.wordpress.com/2012/05/12/spring-mvc-portlet-hello-world-based-on-annotations
+ * Here you will use the Annotated Classes for Spring Framework 2.5.x, in this portlet Spring Framework 3.2.3.RELEASE is used.
+ * You can also follow the documentation contained in the official Spring Framework doc site.
+ * http://static.springsource.org/spring/docs/3.2.x/spring-framework-reference/html/portlet.html
+ */
 package com.liferay.sample.hello;
 
 import javax.portlet.RenderRequest;
